24390cee174601d86e711b3649f449793ea79cef
Farkas Csaba <fcsabi789@gmail.com>
2023-03-20 18:28:22 +0100

    debug replace

---
+++ angularproject/src/app/admin/products/products.component.ts
@@ -0,0 +1,45 @@
+import { productModel } from './../../models/product.model';
+import { ProductService } from 'src/app/services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any
+declare var toastr: any;
+declare var Toast: any;
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+products!: productModel[]; 
+// For Image 
+SelectedImage:File=null as any;
+
+  constructor(private proser:ProductService) {
+
+  }
+
+  ngOnInit(): void {
+this.proser.AllProducts.subscribe(res=>{
+  
+  this.products=res
+console.log(this.products);
+
+});
+
+  }
+
+  search(input: any)
+  {
+    this.proser.getFromDb(input);
+  }
+
+  onSelect(event: any)
+  {
+    var tmppath = URL.createObjectURL(event.target.files[0]);
+    $("#AddEmpImage").fadeIn("fast").attr('src',tmppath);
+    this.SelectedImage=<File>event.target.files[0];
+  }
+}
+
